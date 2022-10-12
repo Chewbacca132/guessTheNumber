1000feb149b500220151df53d28f9f432c965e95
Chewbacca132 <krystof.bresky@zaci.gymnazium-opatov.cz>
2022-10-12 20:10:53 +0200

    add README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+A guess the number app made for a React Native course.
+
+
 Clone the repo
 ```git clone https://github.com/Chewbacca132/guessTheNumber.git```
 
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -10,7 +10,7 @@ export default function Home(){
     return (
         <View>
             <Title/>
-            <CustomButton 
+            <Button 
                 title = 'Play'
                 onPress = {() => Nav.navigate('Play', {
                     min : 1,
